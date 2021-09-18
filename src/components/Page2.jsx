6259537d58810ebb8ebf44b1782336529bb02cb4
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-09-19 04:36:05 +0530

    image added

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Box from './Box';
-import Header from './Header';
-import Main from './Main';
+import React from "react";
+import Box from "./Page2/Box";
+import Header from "./Page2/Header";
+import Main from "./Page2/Main";
 
 const Page2 = () => {
-    return (
-        <div>
-            <Header/>
-            <Main/>
-            <Box/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Box />
+    </div>
+  );
+};
 
 export default Page2;
---
+++ src/components/Page2/Box.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Box = () => {
+  return (
+    <>
+      <div
+        className="d-flex justify-content-around"
+        style={{ margin: "10%", border: "1px solid gray" }}
+      ></div>
+    </>
+  );
+};
+
+export default Box;
