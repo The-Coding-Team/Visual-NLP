962231e2623b711eb32f1d26f27898c70eb8120f
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-09-19 05:53:01 +0530

    working on css

--- src/components/Page2/Box.jsx
+++ src/components/Page2/Box.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import Reference from "../../assets/images/adhar1.jpeg";
-// import TextField from "@mui/material/TextField";
+import Reference from "../../assets/images/ref-img.jpeg";
 
 const Box = () => {
   return (
@@ -13,20 +12,18 @@ const Box = () => {
         <img src={Reference} alt="werr" />
         <div className="text-main--container">
           <Card className="m-2 p-3">
-            <Card.Title>Page 1</Card.Title>
+            <Card.Title>
+              <span>Page 1</span>
+            </Card.Title>
             <Card>
-              <Card.Title>Block 1</Card.Title>
-              <Card className="mx-2 mb-2">
-                <Card.Title>Paragraph 1</Card.Title>
+              <Card.Title>
+                <span>Block 1</span>
+              </Card.Title>
+              <Card className="paragraph-card mx-2 mb-2">
+                <Card.Title>
+                  <span>Paragraph 1</span>
+                </Card.Title>
                 <Card.Body>Text</Card.Body>
-                {/* <TextField
-                  id="outlined-read-only-input"
-                  label="Read Only"
-                  defaultValue="Hello World"
-                  InputProps={{
-                    readOnly: true,
-                  }}
-                /> */}
               </Card>
             </Card>
             <Card>
